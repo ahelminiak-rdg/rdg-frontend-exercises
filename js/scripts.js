30293c69fba33284628aa2a3b8b8606a7931a357
Caitlyn Hain <hainc1@ferris.edu>
2018-08-07 15:11:50 -0400

    jquery exercuse

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-    // your code here
+  $( ".collapsed" ).click(function() {
+		$(this).toggleClass('expanded');
+		return false;
+  });
+
+$('.exercise-1-link').click(function() {
+		console.log($(this).text());
+});
+	
+$('.slider').slick({
+		infinite: true,
+	});
+
+$('.check-class').click(function() {
+	if ($(this).hasClass('highlighted')){
+		$(this).removeClass('highlighted');
+		console.log("TRUE");
+	} else {
+		console.log("FALSE");
+	}
+});
+
+
+		
+
 });
